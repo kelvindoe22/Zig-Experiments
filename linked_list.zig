3c1e6fc3d9475af8c3a7107be9ddcc3ae8b9a600
kelvindoe22 <kelvindoe22@gmail.com>
2022-02-14 22:34:56 +0000

    generic linked list

--- linked_list.zig
+++ linked_list.zig
@@ -1,29 +1,51 @@
 const std = @import("std");
 const allocator = std.heap.page_allocator;
 
-const List = struct {
-    val : i32,
-    next: ?*List,
 
-    pub fn new(val: i32) List{
-        return List {
+
+pub fn main() !void{
+
+    const linked = make_list(u8);
+    var linked_list = linked.new(5);
+    try linked_list.add(4);
+    try linked_list.add(3);
+    try linked_list.add(2);
+    try linked_list.add(1);
+    
+
+    linked.print_all();
+}
+
+
+fn make_list(comptime T: type) type {
+    return struct {
+    const Self = @This();
+    val : T,
+    next: ?*Self,
+
+    
+
+    pub fn new(val: T) Self{
+        return Self {
             .val = val,
             .next = null,
         };
     }
 
-    pub fn add(self: *List, val: i32) !void {
+    pub fn add(self: *Self, val: T) !void {
         var a = self;
         while (a.next != null ) {
             a = a.next.?;
         }
-        var temp = try allocator.create(List);
+        var temp = try allocator.create(Self);
         temp.*.val = val;
         temp.*.next = null;
         a.next = temp;
     }
 
-    pub fn print_all(self: List) void {
+    pub fn pop_front(self)
+
+    pub fn print_all(self: Self) void {
         var a = self;
         while (a.next != null) {
             std.debug.print("{} -> ", .{a.val});
@@ -32,20 +54,5 @@ const List = struct {
         std.debug.print("{} \n", .{a.val});
     }
 };
-
-
-
-pub fn main() !void{
-
-    var a = List.new(0);
-    try a.add(1);
-    try a.add(2);
-    try a.add(3);
-    try a.add(4);
-    try a.add(5);
-    try a.add(6);
-    try a.add(7);
-    
-
-    a.print_all();
 }
+
